5431f864bd8efa40e68cd916b1c8418e3c075800
VINOD JADHAV <37527694+vinodjadhavCloud@users.noreply.github.com>
2024-11-25 13:48:38 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,20 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-023ff3d4ab11b2525"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_instance" "node1" {
+  ami           = "ami-023ff3d4ab11b2525"   #change the AMI id according to your preference
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-07384bf44664eb699"     #change the Subnet id according to your preference
+  tags = {
+    Name = "node1"
+  }
+}
+resource "aws_instance" "node2" {
+  ami           = "ami-023ff3d4ab11b2525"      #change the AMI id according to your preference
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-07384bf44664eb699"     #change the Subnet id according to your preference
+  tags = {
+    Name = "node2"
+  }
 }
